c45c463ee39b0c308b76234a1f985c23110be736
kakashi1210 <desmondaditya@gmail.com>
2024-02-11 14:55:30 +0530

    feat:added eslint-prettier

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ app.get("/", (req: Request, res: any) => {
   res.send("This is default route");
 });
 
-app.post("/deploy", async (req: any, res: any) => {
-  console.log(req.body);
-  res.send(`${req.body} this is response from server`);
-});
+// app.post("/deploy", async (req: any, res: any) => {
+//   console.log(req.body);
+//   res.send(`${req.body} this is response from server`);
+// });
 
 app.listen(port, () => {
   console.log("Server is running on - http://localhost:3000");
