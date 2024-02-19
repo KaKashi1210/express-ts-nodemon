24593d7aa86630eb97e982a0c6ee08770c354cdc
kakashi1210 <desmondaditya@gmail.com>
2024-02-20 00:02:58 +0530

    feat: added esbuild for simple ts build

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
-import express from "express";
-import { Request, Response } from "express";
+function hello() {
+  console.log("hello world");
+}
 
-const app = express();
-app.use(express.json());
-const port = 3000;
-
-app.get("/", (req: Request, res: any) => {
-  res.send("This is default route");
-});
-
-app.post("/deploy", async (req: any, res: any) => {
-  console.log(req.body);
-  res.send(`${req.body} this is response from server`);
-});
-
-app.listen(port, () => {
-  console.log("Server is running on - http://localhost:3000");
-});
+hello();
